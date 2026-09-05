c93c15ee862eb3272df5e4f108b4b8152cae545a
Hithesh Vurjana <codehithesh@gmail.com>
2026-09-06 01:08:47 +0530

    leetcode qs for contest prep

---
+++ leetcode_contest_prep_12_categories/04_stack_monotonic_stack/daily_temperatures.py
@@ -0,0 +1,16 @@
+# LeetCode: Daily Temperatures
+# Link: https://leetcode.com/problems/daily-temperatures/
+#
+# TODO: Implement your solution here.
+#
+# Suggested workflow:
+# 1. Read the problem carefully.
+# 2. Identify the pattern/data structure.
+# 3. Write the brute-force idea first.
+# 4. Optimize to meet the constraints.
+# 5. Add your own test cases.
+#
+# Expected LeetCode signature can be copied from the problem page.
+
+class Solution:
+    pass
---
+++ leetcode_contest_prep_12_categories/08_heap_priority_queue/find_median_from_data_stream.py
@@ -0,0 +1,16 @@
+# LeetCode: Find Median from Data Stream
+# Link: https://leetcode.com/problems/find-median-from-data-stream/
+#
+# TODO: Implement your solution here.
+#
+# Suggested workflow:
+# 1. Read the problem carefully.
+# 2. Identify the pattern/data structure.
+# 3. Write the brute-force idea first.
+# 4. Optimize to meet the constraints.
+# 5. Add your own test cases.
+#
+# Expected LeetCode signature can be copied from the problem page.
+
+class Solution:
+    pass
